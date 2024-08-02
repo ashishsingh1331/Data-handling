691d076978ec0209406b7842af2a716f2bde28d7
Ashish singh <ashishsingh1331@gmail.com>
2024-08-02 17:58:55 +0530

    Catch all routes

---
+++ src/app/dashboard/@analytics/[...cathAllPage]/loading.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function CatchAllLoading() {
+  return <div>CatchAllLoading</div>;
+}
---
+++ src/app/dashboard/@analytics/[...cathAllPage]/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function CatchAll() {
+  return <div>CatchAll</div>;
+}
